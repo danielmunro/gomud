2a7ef2b96696350bc860e95cc5b4058b27fe540c
Dan Munro <dmunro@alegion.com>
2015-10-11 17:49:03 -0700

    better project structure... still testing

---
+++ cmd/run/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/danielmunro/gomud"
+)
+
+func main() {
+	server.NewServer(8080).run()
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "./gomud"
-
-func main() {
-	gomud.NewServer(8080).Run()
-}
